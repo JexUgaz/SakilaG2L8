c1831b518dd13b36b952ed6d9436e47b1601ee3c
JoaquinOrellana <joaquin.orellana@pucp.edu.pe>
2022-11-08 21:15:16 -0500

    getter and stters

--- src/main/java/com/example/sakilag2l8/entities/PeliculaEntity.java
+++ src/main/java/com/example/sakilag2l8/entities/PeliculaEntity.java
@@ -1,9 +1,14 @@
 package com.example.sakilag2l8.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "pelicula", schema = "sakilag2", catalog = "")
 public class PeliculaEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,33 +18,4 @@ public class PeliculaEntity {
     @Basic
     @Column(name = "Nombre")
     private String nombre;
-
-    public int getIdPelicula() {
-        return idPelicula;
-    }
-
-    public void setIdPelicula(int idPelicula) {
-        this.idPelicula = idPelicula;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        PeliculaEntity that = (PeliculaEntity) o;
-        return idPelicula == that.idPelicula && Objects.equals(nombre, that.nombre);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(idPelicula, nombre);
-    }
 }
